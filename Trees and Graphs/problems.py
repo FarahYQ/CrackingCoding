54d8dad3d570118a3851218d759e5c1423cf0618
Farah Quader <fyquader@gmail.com>
2019-01-15 23:22:09 -0800

    Create minimal tree and find depth

---
+++ Trees and Graphs/problems.py
@@ -0,0 +1,83 @@
+# Problem 4.1:
+def route(graph, node1, node2):
+  # graph = {1: [3,4,5], 2: [3,4]} --> False
+  # graph = {1:[3,4,5],2:[4],3:[1,2]} --> True
+  visited = set()
+  queue = [node1]
+  while queue:
+    print(queue)
+    curr = queue[0]
+    if curr == node2: return True
+    queue = queue[1:]
+    if curr not in graph: continue
+    for node in graph[curr]:
+      if node in visited: continue
+      queue.append(node)
+    visited.add(curr)
+  return False
+
+graph = {1:[3,4,5],2:[4],3:[1,2]}
+print(route(graph, 1,2))
+
+# Problem 4.2:
+class Node:
+  def __init__(self,val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+class Tree:
+  def __init__(self):
+    return
+
+  def minimal_tree(self,asc_arr):
+    if len(asc_arr) == 0: return None
+    mid = int(len(asc_arr)/2)
+    root = Node(asc_arr[mid])
+    root.left = self.minimal_tree(asc_arr[:mid])
+    root.right = self.minimal_tree(asc_arr[mid+1:])
+    return root
+
+  def bst_traversal(self,root):
+    if root.val == None: return []
+    curr = root
+    curr_idx = 0
+    queue = [root]
+    while curr.left or curr.right:
+      if curr.left: queue.append(curr.left)
+      if curr.right: queue.append(curr.right)
+      curr_idx += 1
+      curr = queue[curr_idx]
+    order = []
+    for node in queue:
+      order.append(node.val)
+    return order
+
+  def depth(self,root):
+    if root.val == None: return []
+    queue = [root]
+    new_root = Node(root.val)
+    all_levels = [new_root]
+    next_level = []
+    head = Node(None)
+    temp = head
+    while queue:
+      curr = queue[0]
+      queue = queue[1:]
+      if curr.left: next_level.append(curr.left)
+      if curr.right: next_level.append(curr.right)
+      if not queue:
+        queue = next_level
+        next_level = []
+        for node in queue:
+          new_node = Node(node.val)
+          temp.next = new_node
+          temp = new_node
+        all_levels.append(head.next)
+        temp = head
+    return all_levels
+
+tree = Tree()
+root = tree.minimal_tree([1,2,3,4,7,9])
+print(tree.bst_traversal(root))
+
